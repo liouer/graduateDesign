2d592d6b427b1e5037e85037f53ade219711107c
LioLiu <365724120@qq.com>
2020-10-22 19:24:21 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,200 @@
+<template>
+  <div id="app">
+    <h1>{{ todolistTitle }}</h1>
+    <div class="">
+      <div class="">
+        <input
+          type="checkbox"
+          id="allCheck"
+          v-model="allEvents.isSelect"
+          @change="allSelect()"
+        />
+        <label for="allCheck"> </label>
+        <!-- <span>{{ allEvents.text }}</span> -->
+        <input
+          class="input_text"
+          v-model="newEventText"
+          placeholder="请输入代办事项"
+        />
+        <!-- 全选框 -->
+        <div style="display: inline">
+          <button class="head_btn" @click="addEvent">新增</button>
+          <button class="head_btn" @click="delEvents()">删除</button>
+        </div>
+      </div>
+
+      <div class="">
+        <ul>
+          <li v-for="(item, index) in eventList" :key="item.id">
+            <input
+              type="checkbox"
+              name=""
+              id="singleCheck"
+              v-model="item.isSelect"
+              @change="singleSelect()"
+            />
+            <!-- 使用组件，并传参 -->
+            <HelloWorld
+              :index="index"
+              :item="item"
+              @delEvent="delEvent"
+            ></HelloWorld>
+                      <!-- <div class="clean"></div> -->
+
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "@/components/HelloWorld.vue"; //引入组件
+export default {
+  components: { HelloWorld }, //注册组件
+  data() {
+    return {
+      todolistTitle: "TO DO LIST BY Liou",
+      allEvents: {
+        isSelect: false,
+        text: " ",
+      },
+      newEventText: "",
+      eventList: [
+        {
+          id: 1,
+          isSelect: false,
+          eventText: "吃饭",
+        },
+        {
+          id: 2,
+          isSelect: false,
+          eventText: "睡觉",
+        },
+      ],
+      newEventId: 3,
+    };
+  },
+  methods: {
+    // 新增按钮
+    addEvent() {
+      console.log("click add");
+      this.eventList.push({
+        id: this.newEventId++,
+        eventText: this.newEventText,
+        isSelect: false,
+      }),
+        console.dir(this.eventList);
+        console.log(this.newEventId);
+    },
+    // 删除单个按钮
+    delEvent(index) {
+      // alert(index);
+      this.eventList.splice(index, 1);
+      console.log(index);
+    },
+    //批量删除按钮
+    delEvents() {
+      //定义一个数组，存放原数组中没有被选中的元素
+      let arr = [];
+      //筛选元素组中没有被选中的元素然后push到新数组arr中
+      for (let i = 0; i < this.eventList.length; i++) {
+        if (!this.eventList[i].isSelect) {
+          arr.push(this.eventList[i]);
+        }
+      }
+      //将新的数组赋值给原数组
+      this.eventList = arr;
+    },
+    // 全选复选框
+    allSelect() {
+      this.eventList.forEach((item) => {
+        item.isSelect = this.allEvents.isSelect;
+      });
+    },
+    // 单选复选框
+    singleSelect() {
+      //定义一个n来存储用户选了多少个复选框以作判断
+      let n = 0; // eslint-disable-line no-unused-vars
+      this.eventList.forEach((item, index) => {
+        if (item.isSelect) {
+          n++;
+          console.log("index :>> ", index);
+        }
+      });
+      //若用户在全选了，则全选的勾也打上
+      if (n == this.eventList.length) {
+        this.allEvents.isSelect = true;
+      }
+      //在全选过后不全选时，取消全选勾
+      else {
+        this.allEvents.isSelect = false;
+      }
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+ul {
+  padding: 0;
+}
+li {
+  text-decoration: underline;
+  list-style: none;
+  margin-bottom: 5px;
+}
+button {
+  margin-right: 10px;
+  padding: 5px 23px;
+  border-radius: 3px;
+}
+.input_text {
+  width: 20%;
+  margin-right: 10px;
+  padding: 7px;
+  outline: none;
+  border: 1px solid #9e9e9e;
+}
+/* .read_text {
+  outline: none;
+  border: none;
+  text-decoration: underline;
+  width: 20%;
+  margin-right: 10px;
+  padding: 10px;
+  font-size: 16px;
+} */
+.head_btn {
+  background: white;
+  border: 1px solid #9e9e9e;
+}
+/* .del_btn {
+  color: white;
+  background: #f76666;
+  border: 1px solid #af9b9b;
+} */
+/* .clean{
+  clear: both;
+} */
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="hello">
+    <label for="singleCheck"></label>
+    <input class="read_text" v-model="item.eventText" readonly />
+    <div style="display: inline">
+      <button class="del_btn" @click="del(index)">删除</button>
+      <button class="done_btn" >编辑</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data(){
+    return{
+    }
+  },
+  // 获取参数：   参数名：数据类型
+  props: {
+    item: String,
+    index: Number
+  },
+  methods:{
+    // 删除单个按钮
+    del(index) {
+      this.$emit('delEvent',index)          //触发父组件的delEvent方法，并将index传过去
+    },
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+/* .input_text {
+  width: 20%;
+  margin-right: 10px;
+  padding: 7px;
+  outline: none;
+  border: 1px solid #9e9e9e;
+} */
+.read_text {
+  outline: none;
+  border: none;
+  text-decoration: underline;
+  width: 20%;
+  margin-right: 10px;
+  padding: 10px;
+  font-size: 16px;
+}
+/* .head_btn {
+  background: white;
+  border: 1px solid #9e9e9e;
+} */
+.del_btn {
+  color: white;
+  background: #f76666;
+  border: 1px solid #af9b9b;
+}
+/* .clean{
+  clear: both;
+} */
+</style>
